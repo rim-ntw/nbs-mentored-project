e248294436a35ae9af8d360d236fc62a1a833afd
rim-ntw <rimma.chepik@nationwide.co.uk>
2020-10-15 18:42:45 +0100

    updated scripts

--- service-1/scripts/build.sh
+++ service-1/scripts/build.sh
@@ -1,11 +1,3 @@
 #! /bin/bash
 
-#sudo docker build -t localhost:5001/service2 .
-
-#sudo docker build -t localhost:5002/service3 .
-
-#sudo docker build -t localhost:5003/service4 .
-
-#sudo docker build -t localhost:8080/nbs-mentored-project .
-
 sudo docker build -f service-1/Dockerfile -t localhost:8082/service1 .
--- service-1/scripts/push.sh
+++ service-1/scripts/push.sh
@@ -1,13 +1,5 @@
 #! /bin/bash/
 
-#sudo docker push localhost:5001/service2 
-
-#sudo docker push localhost:5002/service3
-
-#sudo docker push localhost:5003/service4
-
-#sudo docker push localhost:8080/nbs-mentored-project
-
 sudo docker push localhost:8082/service1
 
 sudo docker pull localhost:8082/service1
