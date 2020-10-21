bcbd12e2aa82bc44ed164ff848b04b22d631e9ab
rim-ntw <71330911+rim-ntw@users.noreply.github.com>
2020-10-21 17:40:46 +0100

    Update build_services.sh

--- scripts/build_services.sh
+++ scripts/build_services.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
+#sudo docker build -t localhost:8082/service_1:${BUILD_NUMBER} ./service-1
 
-sudo docker build -t localhost:8082/service_1:${BUILD_NUMBER} ./service-1
-sudo docker build -t localhost:8082/service_2:${BUILD_NUMBER} ./service-2
-sudo docker build -t localhost:8082/service_3:${BUILD_NUMBER} ./service-3
-sudo docker build -t localhost:8082/service_4:${BUILD_NUMBER} ./service-4
+
+sudo docker build -t localhost:8082/service_1 ./service-1
+sudo docker build -t localhost:8082/service_2 ./service-2
+sudo docker build -t localhost:8082/service_3 ./service-3
+sudo docker build -t localhost:8082/service_4 ./service-4
 
