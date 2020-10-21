f70b339ef6b7ec83507ebf228fe27dae609a1d2d
rim-ntw <71330911+rim-ntw@users.noreply.github.com>
2020-10-21 17:14:21 +0100

    Update push_services.sh

--- scripts/push_services.sh
+++ scripts/push_services.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-sudo docker push localhost:8082/service_1
-sudo docker push localhost:8082/service_2
-sudo docker push localhost:8082/service_3
-sudo docker push localhost:8082/service_4
+sudo docker push localhost:8082/service_1 ./service-1
+sudo docker push localhost:8082/service_2 ./service-2
+sudo docker push localhost:8082/service_3 ./service-3
+sudo docker push localhost:8082/service_4 ./service-4
 
 
