b7283cf84a6b5388fd9ec3921256557761ac5c00
rim-ntw <71330911+rim-ntw@users.noreply.github.com>
2020-10-30 14:16:26 +0000

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,9 @@ python3 -m venv venv
 . ./venv/bin/activate
 pip3 install -r requirements.txt
 
-pytest ./service-1
-pytest ./service-2
-pytest ./service-3
-pytest ./service-4
+pytest ./service-1 --cov ./service-1/application/
+pytest ./service-2 --cov ./service-2/application/
+pytest ./service-3 --cov ./service-3/application/
+pytest ./service-4 --cov ./service-4/application/
 
 rm -rf venv
