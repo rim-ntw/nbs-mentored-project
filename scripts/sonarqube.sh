cfdb035bccf9b2db60de90206f3cc52576733247
rim-ntw <71330911+rim-ntw@users.noreply.github.com>
2020-10-30 15:08:37 +0000

    Create sonarqube.sh

---
+++ scripts/sonarqube.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+echo "sonarqube-project \
+  -Dsonar.projectKey=first-project \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=956e4eb2cb800c32e5bf423ea1ae8519ca9b9c66"
