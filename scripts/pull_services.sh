805246137aad958301cd6a581bd0eab0a843f58a
rim-ntw <71330911+rim-ntw@users.noreply.github.com>
2020-10-30 15:11:25 +0000

    Delete pull_services.sh

--- scripts/pull_services.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo docker pull localhost:8082/service_1
-sudo docker pull localhost:8082/service_2
-sudo docker pull localhost:8082/service_3
-sudo docker pull localhost:8082/service_4
